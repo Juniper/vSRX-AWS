dc64a56607c361cc03a0deb0b3507589619c1447
mithunhe <48786169+mithunhe@users.noreply.github.com>
2020-01-13 11:58:35 +0530

    Edited the key name

--- CLI_tool/launch.sh
+++ CLI_tool/launch.sh
@@ -1,6 +1,6 @@
 
 ./vsrx-aws deploy --region us-east-1 \
-                  --key-name mithunh-public-Key \
+                  --key-name <key_name> \
                   --security-group-id sg-067f30261c90c73e4 \
                   --security-group-id sg-0e21b3de087fb50fc \
                   --subnet-id subnet-0fa27de913ebab9ae \
