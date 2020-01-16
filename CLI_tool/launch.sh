2a27c3c711f4d3445867626a20932dd607149c79
mithunhe <48786169+mithunhe@users.noreply.github.com>
2020-01-16 15:56:05 +0530

    Removed unsupported tag

--- CLI_tool/launch.sh
+++ CLI_tool/launch.sh
@@ -1,10 +1,11 @@
 
 ./vsrx-aws deploy --region us-east-1 \
                   --key-name <key_name> \
-                  --security-group-id sg-067f30261c90c73e4 \
-                  --security-group-id sg-0e21b3de087fb50fc \
-                  --subnet-id subnet-0fa27de913ebab9ae \
-                  --profile saml \
+                  --key-file <key-file> \
+                  --vpc-id <vpc-id> \
+                  --nic subnet-name=mgt-01,public-ip=auto \
+                  --nic subnet-name=gw-01,public-ip=auto \
+                  --profile default \
                   ami-06b424cccf70e6a94
 
 
