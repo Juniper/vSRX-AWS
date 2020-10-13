6cc152d8118b7cb60f141194da7b17837c9c2a69
mithunhe <mithunh@juniper.net>
2020-10-13 15:37:44 +0530

    Removing incorrectly named files

--- GuardDuty/SRX-GD-Threatfeed/gd_threatfeed/errors.py
+++
@@ -1,8 +0,0 @@
-#  Copyright ©  2020 Juniper Networks, Inc., All Rights Reserved.
-
-class CloudFeedsError(Exception):
-    """Base error"""
-
-
-class IncorrectVersionError(CloudFeedsError):
-    """Wrong feed version detected."""
